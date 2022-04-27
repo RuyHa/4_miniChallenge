53f0311b2afd08f66dae7219aa95f1c7937fb797
RuyHa <code.ruyha@gmail.com>
2022-04-27 15:28:49 +0900

    xcode 파일 추가

---
+++ ArithmeticOperationGame/ArithmeticOperationGame/ArithmeticOperationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticOperationGameApp.swift
+//  ArithmeticOperationGame
+//
+//  Created by Ruyha on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticOperationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
